8b33d82af174cf7c41267f9afa27039c77f6ec34
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-08-24 12:59:18 +0300

    converted Slides info to objects, updated packages

--- src/Components/Introduction/MainSlider.js
+++ src/Components/Introduction/MainSlider.js
@@ -1,5 +1,6 @@
 import { A11y, Navigation, Pagination, Scrollbar } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { introductionSliderData } from "../../Data/staticData";
 import styles from "./MainSlider.module.scss";
 // Import Swiper styles
 import "swiper/css";
@@ -16,75 +17,26 @@ function MainSlider() {
       slidesPerView={1}
       pagination={{ clickable: true }}
     >
-      <SwiperSlide className={styles.slide}>
-        <img
-          src={require("../../assets/images/introduction-product1.jpg")}
-          alt="product preview"
-        />
-        <div className={styles.content}>
-          <div className={styles.nameProduct}>
-            <img
-              src={require("../../assets/images/apple-logo.png")}
-              alt="market logo"
-            />
-            <strong>Iphone 14 Series</strong>
-          </div>
+      {introductionSliderData.map(
+        ({ productName, productImg, logoImg, discountText, id }) => (
+          <SwiperSlide className={styles.slide} key={id}>
+            <img src={productImg} alt="product preview" />
+            <div className={styles.content}>
+              <div className={styles.nameProduct}>
+                <img src={logoImg} alt="market logo" />
+                <strong>{productName}</strong>
+              </div>
 
-          <h2 className={styles.discount}>Up to 10% off Voucher</h2>
+              <h2 className={styles.discount}>{discountText}</h2>
 
-          <button type="button" className={styles.shopNow}>
-            <a href="\#">Shop Now</a>
-            <i className="fa-solid fa-arrow-right-long"></i>
-          </button>
-        </div>
-      </SwiperSlide>
-
-      <SwiperSlide className={styles.slide}>
-        <img
-          style={{ transform: "scale(1.2)", width: "300px" }}
-          src={require("../../assets/images/introduction-product2.jpg")}
-          alt="product preview"
-        />
-        <div className={styles.content}>
-          <div className={styles.nameProduct}>
-            <img
-              src={require("../../assets/images/canon-logo.png")}
-              alt="market logo"
-            />
-            <strong>Canon EOS 5D MkII</strong>
-          </div>
-
-          <h2 className={styles.discount}>Up to 30% off Voucher</h2>
-
-          <button type="button" className={styles.shopNow}>
-            <a href="\#">Shop Now</a>
-            <i className="fa-solid fa-arrow-right-long"></i>
-          </button>
-        </div>
-      </SwiperSlide>
-
-      <SwiperSlide className={styles.slide}>
-        <img
-          src={require("../../assets/images/Introduction-product3.jpg")}
-          alt="product preview"
-        />
-        <div className={styles.content}>
-          <div className={styles.nameProduct}>
-            <img
-              src={require("../../assets/images/apple-logo.png")}
-              alt="market logo"
-            />
-            <strong>MacBook Pro 16</strong>
-          </div>
-
-          <h2 className={styles.discount}>Up to 15% off Voucher</h2>
-
-          <button type="button" className={styles.shopNow}>
-            <a href="\#">Shop Now</a>
-            <i className="fa-solid fa-arrow-right-long"></i>
-          </button>
-        </div>
-      </SwiperSlide>
+              <button type="button" className={styles.shopNow}>
+                <a href="\#">Shop Now</a>
+                <i className="fa-solid fa-arrow-right-long"></i>
+              </button>
+            </div>
+          </SwiperSlide>
+        )
+      )}
     </Swiper>
   );
 }
