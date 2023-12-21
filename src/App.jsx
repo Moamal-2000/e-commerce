95cb07b121bceaf213f5fc73bdc97a234bba5561
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-12-21 23:54:38 +0300

    test failed, back to previous structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import GlobalOverlay from "./Components/Shared/GlobalOverlay";
-import Home from "./components/homePage/Home";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <div className="App">
       <GlobalOverlay />
-      <Home />
-      {/* <AppRoutes /> */}
+      <AppRoutes />
     </div>
   );
 }
