687711817389467dc96d874c6cd6f02a5a7f2826
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-20 09:35:12 +0300

    removed postcss, seems the production code already using auto prefixer

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import autoprefixer from "autoprefixer";
-import postcss from "postcss";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    postcss({
-      plugins: [autoprefixer],
-      config: "./postcss.config.cjs",
-    }),
-  ],
-  build: {
-    sourcemap: true,
-  },
-});
-
-
-
+  plugins: [react()],
+})
