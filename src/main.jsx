34b9154d7966003e2d2a0e011af97f002b571485
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-20 08:18:06 +0300

    fixing deployment #2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import { store } from "./app/store.jsx";
+import { store } from "./App/store.jsx";
 import "./Styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
