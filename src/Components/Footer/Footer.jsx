98645b1ae8b1e02a3fb247ef2293c6ca2a31f844
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-03-02 18:06:45 +0300

    fully responsive footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,96 +5,98 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.sections}>
-        <section className={styles.section1}>
-          <b>Exclusive</b>
-          <span>Subscribe</span>
-          <p>Get 10% off your first order</p>
+      <div className="container">
+        <div className={styles.sections}>
+          <section className={styles.section1}>
+            <b>Exclusive</b>
+            <span>Subscribe</span>
+            <p>Get 10% off your first order</p>
 
-          <div className={styles.input}>
-            <input type="email" placeholder="Enter your email" id="email" />
-            <label htmlFor="email" title="Send">
-              <SvgIcon name="vector" />
-            </label>
-          </div>
-        </section>
+            <div className={styles.input}>
+              <input type="email" placeholder="Enter your email" id="email" />
+              <label htmlFor="email" title="Send">
+                <SvgIcon name="vector" />
+              </label>
+            </div>
+          </section>
 
-        <section className={styles.section2}>
-          <b>Support</b>
+          <section className={styles.section2}>
+            <b>Support</b>
 
-          <ul>
-            <li>111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.</li>
-            <li>exclusive@gmail.com</li>
-            <li>+88015-88888-9999</li>
-          </ul>
-        </section>
+            <ul>
+              <li>111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.</li>
+              <li>exclusive@gmail.com</li>
+              <li>+88015-88888-9999</li>
+            </ul>
+          </section>
 
-        <section className={styles.section3}>
-          <b>Account</b>
+          <section className={styles.section3}>
+            <b>Account</b>
 
-          <ul>
-            <li>My Account</li>
-            <li>Login / Register</li>
-            <li>Cart</li>
-            <li>Wishlist</li>
-            <li>Shop</li>
-          </ul>
-        </section>
+            <ul>
+              <li>My Account</li>
+              <li>Login / Register</li>
+              <li>Cart</li>
+              <li>Wishlist</li>
+              <li>Shop</li>
+            </ul>
+          </section>
 
-        <section className={styles.section4}>
-          <b>Quick Link</b>
+          <section className={styles.section4}>
+            <b>Quick Link</b>
 
-          <ul>
-            <li>Privacy Policy</li>
-            <li>Terms Of Use</li>
-            <li>FAQ</li>
-            <li>Contact</li>
-          </ul>
-        </section>
+            <ul>
+              <li>Privacy Policy</li>
+              <li>Terms Of Use</li>
+              <li>FAQ</li>
+              <li>Contact</li>
+            </ul>
+          </section>
 
-        <section className={styles.section5}>
-          <b>Download App</b>
-          <p>Save $3 with App New User Only</p>
+          <section className={styles.section5}>
+            <b>Download App</b>
+            <p>Save $3 with App New User Only</p>
 
-          <div className={styles.appInfo}>
-            <div className={styles.qrCode}>
-              <img src={qrCode} alt="QR Code" />
+            <div className={styles.appInfo}>
+              <div className={styles.qrCode}>
+                <img src={qrCode} alt="QR Code" />
+              </div>
+
+              <div className={styles.downloadButtons}>
+                <button type="button">
+                  <img src={googlePlay} alt="Download app at Google play" />
+                </button>
+
+                <button type="button">
+                  <img src={appStore} alt="Download app at App store" />
+                </button>
+              </div>
             </div>
 
-            <div className={styles.downloadButtons}>
-              <button type="button">
-                <img src={googlePlay} alt="Download app at Google play" />
-              </button>
+            <div className={styles.socialMedia}>
+              <a href="#" title="facebook">
+                <SvgIcon name="facebook" />
+              </a>
 
-              <button type="button">
-                <img src={appStore} alt="Download app at App store" />
-              </button>
+              <a href="#" title="twitter">
+                <SvgIcon name="twitter" />
+              </a>
+
+              <a href="#" title="instagram">
+                <SvgIcon name="instagram" />
+              </a>
+
+              <a href="#" title="linkedin">
+                <SvgIcon name="linkedin" />
+              </a>
             </div>
-          </div>
+          </section>
+        </div>
 
-          <div className={styles.socialMedia}>
-            <a href="#" title="facebook">
-              <SvgIcon name="facebook" />
-            </a>
-
-            <a href="#" title="twitter">
-              <SvgIcon name="twitter" />
-            </a>
-
-            <a href="#" title="instagram">
-              <SvgIcon name="instagram" />
-            </a>
-
-            <a href="#" title="linkedin">
-              <SvgIcon name="linkedin" />
-            </a>
-          </div>
-        </section>
+        <p className={styles.copyRights}>
+          <span>&copy;</span> Copyright Rimel 2022. All right reserved
+        </p>
       </div>
-
-      <p className={styles.copyRights}>
-        <span>&copy;</span> Copyright Rimel 2022. All right reserved
-      </p>
     </footer>
   );
 };
