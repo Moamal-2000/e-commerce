07f477a3d2911142953f1c022827878a1c40ab10
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-03-17 00:01:03 +0300

    done of EditProfileForm structure and styling

--- src/Components/AccountPage/EditProfileForm.jsx
+++ src/Components/AccountPage/EditProfileForm.jsx
@@ -1,8 +1,74 @@
-import s from "./EditProfileForm.module.scss"
+import s from "./EditProfileForm.module.scss";
 
 const EditProfileForm = () => {
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
   return (
-    <div>EditProfileForm</div>
-  )
-}
-export default EditProfileForm
+    <form className={s.profileForm} onSubmit={handleSubmit}>
+      <h2>Edit Your Profile</h2>
+
+      <section className={s.inputs}>
+        <section className={s.wrapper}>
+          <div className={s.input}>
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" placeholder="Md" />
+          </div>
+
+          <div className={s.input}>
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" placeholder="Rimel" />
+          </div>
+
+          <div className={s.input}>
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" placeholder="rimel1111@gmail.com" />
+          </div>
+
+          <div className={s.input}>
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              id="address"
+              placeholder="Kingston, 5236, United State"
+            />
+          </div>
+        </section>
+
+        <section className={s.passwordInputs}>
+          <div className={s.input}>
+            <label htmlFor="currentPass">Password Changes</label>
+            <input
+              type="currentPass"
+              id="currentPass"
+              placeholder="Current Password"
+            />
+          </div>
+
+          <div className={s.input}>
+            <input type="password" id="newPass" placeholder="New Password" />
+          </div>
+
+          <div className={s.input}>
+            <input
+              type="password"
+              id="confirmPass"
+              placeholder="Confirm New Passwod"
+            />
+          </div>
+        </section>
+      </section>
+
+      <div className={s.buttons}>
+        <button type="reset" className={s.cancelButton}>
+          Cancel
+        </button>
+        <button type="submit" className={s.submitButton}>
+          Save Changes
+        </button>
+      </div>
+    </form>
+  );
+};
+export default EditProfileForm;
