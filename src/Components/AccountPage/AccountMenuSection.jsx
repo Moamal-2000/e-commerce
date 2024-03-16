1b1522d70f35ed967495f6b95fc0bab0c3dd0a66
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-03-16 23:26:59 +0300

    done of AccountMenuSection

--- src/Components/AccountPage/AccountMenuSection.jsx
+++ src/Components/AccountPage/AccountMenuSection.jsx
@@ -1,8 +1,42 @@
-import s from "./AccountMenuSection.module.scss"
+import { NavLink } from "react-router-dom";
+import s from "./AccountMenuSection.module.scss";
 
 const AccountMenuSection = () => {
   return (
-    <section>AccountMenuSection</section>
-  )
-}
-export default AccountMenuSection
+    <section className={s.menuSection}>
+      <section className={s.section}>
+        <h2>Manage My Account</h2>
+
+        <ul>
+          <li>
+            <NavLink to="/profile">My Profile</NavLink>
+          </li>
+
+          <li>
+            <a href="#">Address Book</a>
+          </li>
+
+          <li>
+            <a href="#">My Payment Options</a>
+          </li>
+        </ul>
+      </section>
+
+
+      <section className={s.section}>
+        <h2>My Orders</h2>
+
+        <ul>
+          <li>
+            <a href="#">My Returns</a>
+          </li>
+
+          <li>
+            <a href="#">My Cancellations</a>
+          </li>
+        </ul>
+      </section>
+    </section>
+  );
+};
+export default AccountMenuSection;
