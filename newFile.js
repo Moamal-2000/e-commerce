883581600a3ebbbb17df19e998ccc0a2e5c5238b
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-03-17 08:00:37 +0300

    added postcss autoprefixer

---
+++ newFile.js
@@ -0,0 +1,7 @@
+// postcss.config.js
+module.exports = {
+  plugins: [
+    require("autoprefixer"),
+    // Add other PostCSS plugins here if needed
+  ],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      config: {
+        path: "./postcss.config.cjs",
+      },
+    },
+  },
+});
