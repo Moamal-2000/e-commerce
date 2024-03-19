ea36102a560d3df85e70783fbf6ed7e58071f934
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-03-19 03:28:36 +0300

    fixed LanguageSelector menu

--- src/Components/Header/LanguageSelector.jsx
+++ src/Components/Header/LanguageSelector.jsx
@@ -4,38 +4,32 @@ import s from "./LanguageSelector.module.scss";
 
 const LanguageSelector = () => {
   const [isLangMenuActive, setIsLangMenuActive] = useState(false);
-  const optionsEleRef = useRef();
+  const LANGUAGES = ["English", "Russian", "Arabic"];
   const currentLangRef = useRef();
 
-  function handleLanguageMenu(e) {
-    const target = e.target;
-    const triggeredLanguage = target.textContent;
-    const currentLangEle = currentLangRef.current;
-    const isSvgOrPath = target.tagName === "svg" || target.tagName === "path";
+  function selectLanguage(index) {
+    const selectedLanguage = LANGUAGES[index];
+    currentLangRef.current.textContent = selectedLanguage;
+    // Call a function to change website's language
+  }
 
-    //! Require fix
-    if (isSvgOrPath) {
-      setIsLangMenuActive((prevState) => !prevState);
-      return;
-    }
-
-    if (isLangMenuActive) currentLangEle.textContent = triggeredLanguage;
+  function toggleLanguageMenu(e) {
     setIsLangMenuActive((prevState) => !prevState);
   }
+
   return (
-    <div className={s.languageSelector} onClick={(e) => handleLanguageMenu(e)}>
+    <div className={s.languageSelector} onClick={(e) => toggleLanguageMenu(e)}>
       <div className={s.currentOption} ref={currentLangRef}>
         English
       </div>
       <SvgIcon name="chevronDown" />
 
-      <div
-        className={`${s.options} ${isLangMenuActive ? s.active : ""}`}
-        ref={optionsEleRef}
-      >
-        <div className={s.option}>English</div>
-        <div className={s.option}>Russian</div>
-        <div className={s.option}>Arabic</div>
+      <div className={`${s.options} ${isLangMenuActive ? s.active : ""}`}>
+        {LANGUAGES.map((lang, i) => (
+          <div key={i} className={s.option} onClick={() => selectLanguage(i)}>
+            {lang}
+          </div>
+        ))}
       </div>
     </div>
   );
