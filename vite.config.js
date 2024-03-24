938d310a947c9e275c4af511c070315009375137
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-03-24 12:38:41 +0300

    uninstalled vite-plugin-pwa, trying to setup pwa without the plugin

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,10 @@ import react from "@vitejs/plugin-react-swc";
 import autoprefixer from "autoprefixer";
 import postcss from "postcss/lib/postcss";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
-import { manifestForPlugin } from "./manifest";
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ mode: "none", injectRegister: false }, manifestForPlugin),
     postcss({
       plugins: [autoprefixer],
       config: "./postcss.config.cjs",
