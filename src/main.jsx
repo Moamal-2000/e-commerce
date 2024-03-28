829c39446662f72e093d36c38db50b7ff2ea3d01
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-03-28 08:12:33 +0300

    small letter for registerPWA.js file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
-import * as servicerWorker from "../public/registerPWA.js";
+import * as servicerWorker from "../public/register-pwa.js";
 import App from "./App.jsx";
 import { store } from "./App/store.jsx";
 import "./Styles/main.scss";
