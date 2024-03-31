0afa6d544b26ce2c65ea42af87051dde05fca65f
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-03-31 11:33:10 +0300

    separated roots with external file

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -5,41 +5,9 @@ import {
   createBrowserRouter,
   createRoutesFromChildren,
 } from "react-router-dom";
-import About from "../Components/About/About";
-import AccountPage from "../Components/AccountPage/AccountPage";
-import Cart from "../Components/Cart/Cart";
-import CheckoutPage from "../Components/CheckoutPage/CheckoutPage";
-import Contact from "../Components/Contact/Contact";
-import FavoritePage from "../Components/FavoritePage/FavoritePage";
-import Home from "../Components/Home/Home";
-import LogIn from "../Components/LogIn/LogIn";
-import NotFoundPage from "../Components/NotFoundPage/NotFoundPage";
-import ProductDetailsPage from "../Components/ProductDetailsPage/ProductDetailsPage";
-import ProductsCategoryPage from "../Components/ProductsCategory/ProductsCategoryPage";
-import ProductsPage from "../Components/ProductsPage/ProductsPage";
-import SignUp from "../Components/SignUp/SignUp";
-import WishList from "../Components/WishList/WishList";
 import RequiredAuth from "./RequiredAuth";
 import RoutesLayout from "./RoutesLayout";
-import SignUpWithGoogle from "../Components/SignUp/SignUpWithGoogle/SignUpWithGoogle";
-
-const ROUTES_CONFIG = [
-  { path: "/", element: <Home /> },
-  { path: "/contact", element: <Contact /> },
-  { path: "/about", element: <About /> },
-  { path: "/details", element: <ProductDetailsPage /> },
-  { path: "/category", element: <ProductsCategoryPage /> },
-  { path: "/products", element: <ProductsPage /> },
-  { path: "/favorites", element: <FavoritePage /> },
-  { path: "/wishlist", element: <WishList /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/checkout", element: <CheckoutPage /> },
-  { path: "/signup", element: <SignUp /> },
-  { path: "/login", element: <LogIn /> },
-  { path: "/profile", element: <AccountPage /> },
-  { path: "*", element: <NotFoundPage /> },
-  { path: "/google", element: <SignUpWithGoogle /> },
-];
+import { ROUTES_CONFIG } from "./roots";
 
 const AppRoutes = () => {
   const routes = createRoutesFromChildren(
---
+++ src/Routes/roots.jsx
@@ -0,0 +1,31 @@
+import About from "../Components/About/About";
+import AccountPage from "../Components/AccountPage/AccountPage";
+import Cart from "../Components/Cart/Cart";
+import CheckoutPage from "../Components/CheckoutPage/CheckoutPage";
+import Contact from "../Components/Contact/Contact";
+import FavoritePage from "../Components/FavoritePage/FavoritePage";
+import Home from "../Components/Home/Home";
+import LogIn from "../Components/LogIn/LogIn";
+import NotFoundPage from "../Components/NotFoundPage/NotFoundPage";
+import ProductDetailsPage from "../Components/ProductDetailsPage/ProductDetailsPage";
+import ProductsCategoryPage from "../Components/ProductsCategory/ProductsCategoryPage";
+import ProductsPage from "../Components/ProductsPage/ProductsPage";
+import SignUp from "../Components/SignUp/SignUp";
+import WishList from "../Components/WishList/WishList";
+
+export const ROUTES_CONFIG = [
+  { path: "/", element: <Home /> },
+  { path: "/contact", element: <Contact /> },
+  { path: "/about", element: <About /> },
+  { path: "/details", element: <ProductDetailsPage /> },
+  { path: "/category", element: <ProductsCategoryPage /> },
+  { path: "/products", element: <ProductsPage /> },
+  { path: "/favorites", element: <FavoritePage /> },
+  { path: "/wishlist", element: <WishList /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/checkout", element: <CheckoutPage /> },
+  { path: "/signup", element: <SignUp /> },
+  { path: "/login", element: <LogIn /> },
+  { path: "/profile", element: <AccountPage /> },
+  { path: "*", element: <NotFoundPage /> },
+];
