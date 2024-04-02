41d06bfc5be0aec65f968a6a4496a5f71fef02aa
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-04-02 16:26:33 +0300

    setup dark mode button

--- src/Features/globalSlice.jsx
+++ src/Features/globalSlice.jsx
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   isOverlayActive: false,
+  isDarkModeActive: false,
   isMobileMenuActive: false,
   isProfileMenuActive: false,
   isSectionsMenuActive: false,
