f7972fc4ce8ea1d6d26896517ba23bd054a60de7
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-04-04 14:22:14 +0300

    testing variable from public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { variable } from "../public/testVariables";
 import useStoreWebsiteDataToLocalStorage from "./Hooks/App/useStoreWebsiteDataToLocalStorage";
 import AppRoutes from "./Routes/AppRoutes";
 
 function App() {
+  console.log(variable);
   useStoreWebsiteDataToLocalStorage();
   return <AppRoutes />;
 }
