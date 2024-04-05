4f39cdc081faf3a21ec077b23f1d6627a31ef4df
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-04-05 12:33:35 +0300

    fixed incorrect spelling and some UI improvements

--- src/Components/About/HeroSection/AboutHeroSection.jsx
+++ src/Components/About/HeroSection/AboutHeroSection.jsx
@@ -8,16 +8,16 @@ const AboutHeroSection = () => {
         <h2>Our Story</h2>
 
         <p>
-          Launced in 2015, Exclusive is South Asia’s premier online shopping
-          makterplace with an active presense in Bangladesh. Supported by wide
-          range of tailored marketing, data and service solutions, Exclusive has
-          10,500 sallers and 300 brands and serves 3 millioons customers across
-          the region.
+          Launched in 2015, Exclusive is South Asia’s premier online shopping
+          marketplace with an active presence in Bangladesh. Supported by a wide
+          range of tailored marketing, data, and service solutions, Exclusive
+          has 10,500 sellers and 300 brands and serves 3 million customers
+          across the region.
         </p>
 
         <p>
-          Exclusive has more than 1 Million products to offer, growing at a very
-          fast. Exclusive offers a diverse assotment in categories ranging from
+          Exclusive has more than 1 Million products to offer, growing very
+          fast. Exclusive offers a diverse assortment in categories ranging from
           consumer.
         </p>
       </section>
