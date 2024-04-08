75d20619828a387ac39396c7927d5f3a3b7d2af6
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-04-08 18:40:14 +0300

    removed any preloaded content

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,4 @@
 import { Helmet } from "react-helmet-async";
-import { useLocation } from "react-router-dom";
-import { productImg1 } from "src/Assets/Images/Images";
 import useScrollOnMount from "src/Hooks/App/useScrollOnMount";
 import CategoriesSection from "./CategoriesSection/CategoriesSection";
 import FeaturedSection from "./FeaturedSection/FeaturedSection";
@@ -13,16 +11,12 @@ import ThisMonthSection from "./ThisMonthSection/ThisMonthSection";
 import TodaySection from "./TodaySection/TodaySection";
 
 const Home = () => {
-  const { pathname } = useLocation();
   useScrollOnMount(0);
 
   return (
     <>
       <Helmet>
         <title>E-Commercew</title>
-        {/* {pathname === "/" && (
-        )} */}
-        {/* <link rel="preload" as="image" href={productImg1} /> */}
       </Helmet>
 
       <div className={s.home}>
