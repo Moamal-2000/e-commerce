d05ab9e598caecbfa7606e8f65771ed571cc861c
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-04-10 03:13:01 +0300

    testing a new plan of caching pwa

--- public/sw.js
+++ public/sw.js
@@ -1,47 +1,66 @@
 "use strict";
 
-const CACHE_NAME = "e-commerce-v6";
+const CACHE_NAME = "e-commerce-v7";
 const assets = ["/", "/index.html"];
-const self = this
 
 async function caching() {
   const cache = await caches.open(CACHE_NAME);
-  cache.addAll(assets);
+  await cache.addAll(assets);
 }
 
 async function respondFetch(request) {
   const cacheResponse = await caches.match(request);
 
-  const networkResponse = fetch(request.url).then((networkRes) => {
-    return caches.open(CACHE_NAME).then((cache) => {
-      const skipPutResInCache = networkRes.url.includes("chrome-extension");
+  const networkResponse = fetch(request)
+    .then(async (networkRes) => {
+      if (networkRes.ok) {
+        const cache = await caches.open(CACHE_NAME);
+        await cache.put(request, networkRes.clone());
+      }
 
-      if (skipPutResInCache) return networkRes;
-
-      cache.put(request, networkRes.clone());
       return networkRes;
+    })
+    .catch((err) => {
+      console.error("Fetch failed:", err);
     });
-  }).catch(err => {});
 
   return cacheResponse || networkResponse;
 }
 
-function updateLatestCache() {
-  caches.keys().then((keys) => {
-    return Promise.all(
-      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+async function updateCache() {
+  const cache = await caches.open(CACHE_NAME);
+
+  try {
+    const response = await Promise.all(
+      assets.map(async (asset) => {
+        const fetchedResponse = await fetch(asset);
+
+        if (fetchedResponse.ok) {
+          await cache.put(asset, fetchedResponse.clone());
+        }
+        return fetchedResponse;
+      })
     );
-  });
+
+    const cacheKeys = await caches.keys();
+    await Promise.all(
+      cacheKeys
+        .filter((key) => key !== CACHE_NAME)
+        .map((key) => caches.delete(key))
+    );
+
+    return response;
+  } catch (error) {
+    console.error("Failed to update cache:", error);
+  }
 }
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(caching());
-});
+self.addEventListener("install", (event) => event.waitUntil(caching()));
+self.addEventListener("activate", (event) => event.waitUntil(updateCache()));
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(updateLatestCache());
-});
+self.addEventListener("fetch", (event) => {
+  const isGetMethod = event.request.method === "GET";
 
-self.addEventListener("fetch", async (event) => {
   event.respondWith(respondFetch(event.request));
+  if (isGetMethod) event.waitUntil(updateCache());
 });
