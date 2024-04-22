5328d89031db56ed1a1ad38bfbcf13b537c32a65
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-04-22 10:18:47 +0300

    RemoveCartProductBtn.jsx component

--- src/Components/Cart/CartProducts/CartProduct.jsx
+++ src/Components/Cart/CartProducts/CartProduct.jsx
@@ -1,30 +1,18 @@
-import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { removeById } from "src/Features/productsSlice";
-import SvgIcon from "../../Shared/MiniComponents/SvgIcon";
-import ToolTip from "../../Shared/MiniComponents/ToolTip";
-import s from "./CartProduct.module.scss";
 import CustomNumberInput from "../../Shared/MiniComponents/CustomNumberInput/CustomNumberInput";
+import s from "./CartProduct.module.scss";
+import RemoveCartProductBtn from "./RemoveCartProductBtn";
 
 const CartProduct = ({ data }) => {
   const { img, name, shortName, afterDiscount, quantity, id } = data;
   const subTotal = (quantity * afterDiscount).toFixed(2);
-  const dispatch = useDispatch();
 
   return (
     <tr className={s.productContainer}>
       <td className={s.product}>
         <div className={s.imgHolder}>
           <img src={img} alt={shortName} />
-
-          <button
-            type="button"
-            className={s.removeButton}
-            onClick={() => dispatch(removeById({ key: "cartProducts", id }))}
-          >
-            <SvgIcon name="xMark" />
-            <ToolTip top="50%" left="-44px" content="remove" />
-          </button>
+          <RemoveCartProductBtn productId={id} />
         </div>
 
         <Link to={`/details?product=${name}`}>{shortName}</Link>
---
+++ src/Components/Cart/CartProducts/RemoveCartProductBtn.jsx
@@ -0,0 +1,22 @@
+import { useDispatch } from "react-redux";
+import { removeById } from "../../../Features/productsSlice";
+import SvgIcon from "../../Shared/MiniComponents/SvgIcon";
+import ToolTip from "../../Shared/MiniComponents/ToolTip";
+import s from "./RemoveCartProductBtn.module.scss";
+
+const RemoveCartProductBtn = ({ productId }) => {
+  const dispatch = useDispatch();
+
+  function removeProduct() {
+    const removeAction = removeById({ key: "cartProducts", id: productId });
+    dispatch(removeAction);
+  }
+
+  return (
+    <button type="button" className={s.removeButton} onClick={removeProduct}>
+      <SvgIcon name="xMark" />
+      <ToolTip top="50%" left="-44px" content="remove" />
+    </button>
+  );
+};
+export default RemoveCartProductBtn;
