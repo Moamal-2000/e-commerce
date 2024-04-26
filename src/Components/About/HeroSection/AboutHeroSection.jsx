08bb23a057acd333cf3e1d11a0436f2257a47123
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-04-26 07:47:00 +0300

    translate AboutHeroSection.jsx

--- src/Components/About/HeroSection/AboutHeroSection.jsx
+++ src/Components/About/HeroSection/AboutHeroSection.jsx
@@ -1,31 +1,24 @@
+import { useTranslation } from "react-i18next";
 import { womenShopping } from "src/Assets/Images/Images";
 import s from "./AboutHeroSection.module.scss";
 
 const AboutHeroSection = () => {
+  const { t } = useTranslation();
+
   return (
     <section className={s.heroSection}>
       <section className={s.content}>
-        <h2 tabIndex="0">Our Story</h2>
+        <h2 tabIndex="0">{t("aboutPage.heroSection.heading")}</h2>
 
-        <p tabIndex="0">
-          Launched in 2015, Exclusive is South Asia’s premier online shopping
-          marketplace with an active presence in Bangladesh. Supported by a wide
-          range of tailored marketing, data, and service solutions, Exclusive
-          has 10,500 sellers and 300 brands and serves 3 million customers
-          across the region.
-        </p>
-
-        <p tabIndex="0">
-          Exclusive has more than 1 Million products to offer, growing very
-          fast. Exclusive offers a diverse assortment in categories ranging from
-          consumer.
-        </p>
+        <p tabIndex="0">{t("aboutPage.heroSection.paragraphOne")}</p>
+        <p tabIndex="0">{t("aboutPage.heroSection.paragraphTwo")}</p>
       </section>
 
       <div className={s.imgHolder}>
-        <img src={womenShopping} alt="two young women happy with shopping" />
+        <img src={womenShopping} alt={t("aboutPage.heroSection.imageAlt")} />
       </div>
     </section>
   );
 };
+
 export default AboutHeroSection;
