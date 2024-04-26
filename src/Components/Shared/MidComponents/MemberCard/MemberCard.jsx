3dec35c7c486feb3d1ef034e9d0be2d2c2cd05dd
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-04-26 11:52:01 +0300

    translate MemberCard.jsx for about page

--- src/Components/Shared/MidComponents/MemberCard/MemberCard.jsx
+++ src/Components/Shared/MidComponents/MemberCard/MemberCard.jsx
@@ -1,9 +1,13 @@
+import { useTranslation } from "react-i18next";
+import { camelCase } from "../../../../Functions/helper";
 import SvgIcon from "../../MiniComponents/SvgIcon";
 import ToolTip from "../../MiniComponents/ToolTip";
 import s from "./MemberCard.module.scss";
 
 const MemberCard = ({ data }) => {
+  const { t } = useTranslation();
   const { name, jobTitle, img, socialMedia } = data;
+  const memberDataTrans = `aboutPage.ourMembersData.${camelCase(jobTitle)}`;
 
   return (
     <div className={s.card}>
@@ -12,8 +16,8 @@ const MemberCard = ({ data }) => {
       </div>
 
       <div className={s.info}>
-        <b tabIndex="0">{name}</b>
-        <p tabIndex="0">{jobTitle}</p>
+        <b tabIndex="0">{t(`${memberDataTrans}.name`)}</b>
+        <p tabIndex="0">{t(`${memberDataTrans}.jobTitle`)}</p>
 
         <div className={s.socialMedia}>
           <SocialMedia socialMedia={socialMedia} />
@@ -26,9 +30,11 @@ export default MemberCard;
 
 const SocialMedia = ({ socialMedia }) => {
   const socialMediaNames = Object.keys(socialMedia);
+  const { t } = useTranslation();
 
   return socialMediaNames?.map((mediaName, i) => {
     const instagramClass = mediaName === "instagram" ? s.instagram : "";
+    const mediaNameTrans = t(`common.${mediaName}`);
 
     return (
       <a
@@ -36,10 +42,10 @@ const SocialMedia = ({ socialMedia }) => {
         href={socialMedia[mediaName]}
         target="_blank"
         className={instagramClass}
-        title={mediaName}
+        title={mediaNameTrans}
       >
         <SvgIcon name={mediaName} />
-        <ToolTip top="40px" left="50%" content={mediaName} />
+        <ToolTip top="40px" left="50%" content={mediaNameTrans} />
       </a>
     );
   });
