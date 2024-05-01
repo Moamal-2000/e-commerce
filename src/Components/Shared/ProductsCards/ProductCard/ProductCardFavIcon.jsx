de840f82f1f72f43f377ca386bf3dca0dea0ee28
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-05-01 11:32:28 +0300

    separated fav icon into ProductCardFavIcon component

---
+++ src/Components/Shared/ProductsCards/ProductCard/ProductCardFavIcon.jsx
@@ -0,0 +1,54 @@
+import cookies from "js-cookie";
+import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addToArray, removeById } from "src/Features/productsSlice";
+import { favIconToolTipLeftPos } from "src/Functions/componentsFunctions";
+import { isItemFound } from "src/Functions/helper";
+import SvgIcon from "../../MiniComponents/SvgIcon";
+import ToolTip from "../../MiniComponents/ToolTip";
+import s from "./ProductCardFavIcon.module.scss";
+
+const ProductCardFavIcon = ({ product, productId }) => {
+  const { loginInfo } = useSelector((state) => state.user);
+  const { favoritesProducts } = useSelector((state) => state.products);
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const navigateTo = useNavigate();
+  const lang = cookies.get("i18next");
+  const favIconLeftToolTipPos = favIconToolTipLeftPos(lang);
+  const isAddedToFavorites = favoritesProducts?.find(
+    (favProduct) => favProduct.id === productId
+  );
+  const activeClass = isAddedToFavorites ? s.active : "";
+
+  function addProductToFavorite() {
+    const isProductAlreadyExist = isItemFound(favoritesProducts, product, "id");
+    if (!loginInfo.isSignIn) navigateTo("/signup");
+    if (isProductAlreadyExist) {
+      dispatch(removeById({ key: "favoritesProducts", id: product.id }));
+      return;
+    }
+
+    dispatch(addToArray({ key: "favoritesProducts", value: product }));
+  }
+
+  return (
+    <button
+      type="button"
+      className={`${s.iconHolder} ${s.favIcon} ${activeClass}`}
+      onClick={addProductToFavorite}
+      aria-label={t("productCard.icons.favorite")}
+    >
+      <div className={s.heartBackground} />
+
+      <SvgIcon name="heart" />
+      <ToolTip
+        top="18px"
+        left={favIconLeftToolTipPos}
+        content={t("productCard.icons.favorite")}
+      />
+    </button>
+  );
+};
+export default ProductCardFavIcon;
--- src/Components/Shared/ProductsCards/ProductCard/ProductCardIcons.jsx
+++ src/Components/Shared/ProductsCards/ProductCard/ProductCardIcons.jsx
@@ -1,13 +1,5 @@
-import cookies from "js-cookie";
-import { useTranslation } from "react-i18next";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { favIconToolTipLeftPos } from "src/Functions/componentsFunctions";
-import { isItemFound } from "src/Functions/helper";
-import { addToArray, removeById } from "../../../../Features/productsSlice";
-import SvgIcon from "../../MiniComponents/SvgIcon";
-import ToolTip from "../../MiniComponents/ToolTip";
 import ProductCardDetailsIcon from "./ProductCardDetailsIcon";
+import ProductCardFavIcon from "./ProductCardFavIcon";
 import s from "./ProductCardIcons.module.scss";
 import ProductCardRemoveIcon from "./ProductCardRemoveIcon";
 import ProductCardWishListIcon from "./ProductCardWishListIcon";
@@ -19,47 +11,10 @@ const ProductCardIcons = ({
   product,
   removeFrom,
 }) => {
-  const { loginInfo } = useSelector((state) => state.user);
-  const { favoritesProducts } = useSelector((state) => state.products);
-  const isAddedToFavorites = favoritesProducts?.find(
-    (favProduct) => favProduct.id === productId
-  );
-  const { t } = useTranslation();
-  const dispatch = useDispatch();
-  const navigateTo = useNavigate();
-  const lang = cookies.get("i18next");
-  const favIconLeftToolTipPos = favIconToolTipLeftPos(lang);
-
-  function addProductToFavorite() {
-    const isProductAlreadyExist = isItemFound(favoritesProducts, product, "id");
-    if (!loginInfo.isSignIn) navigateTo("/signup");
-    if (isProductAlreadyExist) {
-      dispatch(removeById({ key: "favoritesProducts", id: product.id }));
-      return;
-    }
-
-    dispatch(addToArray({ key: "favoritesProducts", value: product }));
-  }
-
   return (
     <div className={s.icons} data-product-icons-hover>
       {showFavIcon && (
-        <button
-          type="button"
-          className={`${s.iconHolder} ${s.favIcon} ${
-            isAddedToFavorites ? s.active : ""
-          }`}
-          onClick={addProductToFavorite}
-          aria-label={t("productCard.icons.favorite")}
-        >
-          <div className={s.heartBackground}></div>
-          <SvgIcon name="heart" />
-          <ToolTip
-            top="18px"
-            left={favIconLeftToolTipPos}
-            content={t("productCard.icons.favorite")}
-          />
-        </button>
+        <ProductCardFavIcon product={product} productId={productId} />
       )}
 
       {showDetailsIcon && (
