a2fba747b8b0e9332b9656817c8dcc6ff984b71d
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-05-07 20:13:20 +0300

    Show warning alert to login if user visited page require login

--- src/Routes/RequiredAuth.jsx
+++ src/Routes/RequiredAuth.jsx
@@ -1,11 +1,13 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
 import { pagesRequireSignIn } from "../Data/globalVariables";
+import { showAlert } from "../Features/globalSlice";
 
 const RequiredAuth = ({ children }) => {
   const { loginInfo } = useSelector((state) => state.user);
   const { isSignIn } = loginInfo;
   const location = useLocation();
+  const dispatch = useDispatch();
   const pathName = location.pathname;
   const isLoginOrSignUpPage = pathName === "/login" || pathName === "/signup";
 
@@ -13,7 +15,16 @@ const RequiredAuth = ({ children }) => {
     pagesRequireSignIn.includes(page) && !isSignIn;
 
   if (isLoginOrSignUpPage && isSignIn) return <Navigate to="/" />;
-  if (isPageRequiringSignIn(pathName)) return <Navigate to="/signup" />;
+  if (isPageRequiringSignIn(pathName)) {
+    loginFirstAlert();
+    return <Navigate to="/signup" />;
+  }
+
+  function loginFirstAlert() {
+    const alertText = "Please sign in to visit this page";
+    const alertState = "warning";
+    dispatch(showAlert({ alertText, alertState }));
+  }
 
   return children;
 };
