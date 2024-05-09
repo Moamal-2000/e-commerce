c387a045f2e166cf876501c8ec511d071001cb2d
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-05-09 07:38:05 +0300

    simple changes

--- src/Components/Cart/CartInfo/AddCoupon.jsx
+++ src/Components/Cart/CartInfo/AddCoupon.jsx
@@ -1,19 +1,12 @@
-import { useRef } from "react";
 import { useTranslation } from "react-i18next";
 import s from "./AddCoupon.module.scss";
 
 const AddCoupon = () => {
-  const couponInputRef = useRef("");
   const { t } = useTranslation();
 
   return (
     <div className={s.couponContainer}>
-      <input
-        type="text"
-        placeholder={t("inputsPlaceholders.couponExample")}
-        onChange={(e) => (couponInputRef.current = e.target.value)}
-      />
-
+      <input type="text" placeholder={t("inputsPlaceholders.couponExample")} />
       <button type="button">{t("buttons.applyCoupon")}</button>
     </div>
   );
