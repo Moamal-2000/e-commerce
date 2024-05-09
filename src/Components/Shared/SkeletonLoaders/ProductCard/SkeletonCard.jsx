634ac2d258eeb567c53c5ce1f62052981a129520
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-05-09 08:46:31 +0300

    Simplified SkeletonCard and changed loading style

--- src/Components/Shared/SkeletonLoaders/ProductCard/SkeletonCard.jsx
+++ src/Components/Shared/SkeletonLoaders/ProductCard/SkeletonCard.jsx
@@ -3,20 +3,22 @@ import s from "./SkeletonCard.module.scss";
 const SkeletonCard = () => {
   return (
     <div className={s.skeletonCard}>
-      <div className={s.imgHolder}></div>
+      <div className={s.imgHolder} />
 
       <div className={s.productInfo}>
-        <div className={s.title}></div>
+        <div className={s.title} />
+
         <div className={s.priceContainer}>
-          <div className={s.price}></div>
-          <div className={s.afterDiscount}></div>
+          <div className={s.price} />
+          <div className={s.afterDiscount} />
         </div>
+
         <div className={s.rate}>
-          <div className={s.star}></div>
-          <div className={s.star}></div>
-          <div className={s.star}></div>
-          <div className={s.star}></div>
-          <div className={s.star}></div>
+          <div className={s.star} />
+          <div className={s.star} />
+          <div className={s.star} />
+          <div className={s.star} />
+          <div className={s.star} />
         </div>
       </div>
     </div>
