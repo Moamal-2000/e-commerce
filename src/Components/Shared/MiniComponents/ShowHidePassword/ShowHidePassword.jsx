b39614cae87252eab3973e560f5a948ba8b15520
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-06-11 20:41:24 +0300

    separated functions component ShowHidePassword outside

--- src/Components/Shared/MiniComponents/ShowHidePassword/ShowHidePassword.jsx
+++ src/Components/Shared/MiniComponents/ShowHidePassword/ShowHidePassword.jsx
@@ -3,32 +3,19 @@ import { useEffect, useState } from "react";
 import SvgIcon from "../SvgIcon";
 import s from "./ShowHidePassword.module.scss";
 
-const ShowHidePassword = ({ top = "50%", right = "8px" }) => {
+const ShowHidePassword = ({ top = "40%", right = "8px" }) => {
   const [showPass, setShowPass] = useState(true);
   const [iconPosition, setIconPosition] = useState({ top, right });
-
   const lang = cookies.get("i18next");
 
-  function toggleShowPass(e) {
-    const inputWrapperEle = e.currentTarget.parentElement;
-    const inputEle = inputWrapperEle.querySelector("input");
-
-    inputEle.type = inputEle?.type === "password" ? "text" : "password";
-    setShowPass(!showPass);
-  }
-
   useEffect(() => {
-    if (lang === "ar") {
-      setIconPosition({ top, right: "unset", left: right });
-    } else {
-      setIconPosition({ top, left: "unset", right });
-    }
+    updateIconPosition(lang, right, setIconPosition);
   }, [lang]);
 
   return (
     <span
       className={s.iconHolder}
-      onClick={toggleShowPass}
+      onClick={(e) => toggleShowPass(e, showPass, setShowPass)}
       style={iconPosition}
     >
       <SvgIcon name={showPass ? "eye" : "eyeSlash"} />
@@ -36,3 +23,19 @@ const ShowHidePassword = ({ top = "50%", right = "8px" }) => {
   );
 };
 export default ShowHidePassword;
+
+function toggleShowPass(e, showPass, setShowPass) {
+  const inputWrapperEle = e.currentTarget.parentElement;
+  const inputEle = inputWrapperEle.querySelector("input");
+
+  inputEle.type = inputEle?.type === "password" ? "text" : "password";
+  setShowPass(!showPass);
+}
+
+function updateIconPosition(lang, right, setIconPosition) {
+  if (lang === "ar") {
+    setIconPosition({ top, right: "unset", left: right });
+    return;
+  }
+  setIconPosition({ top, left: "unset", right });
+}
