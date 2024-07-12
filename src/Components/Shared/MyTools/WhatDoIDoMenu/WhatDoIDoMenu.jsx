b07af1358c7c0edc4c98e529c63edc5033b0c12b
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-07-12 16:06:32 +0300

    chore: separate WhatDoIDo header into WhatDoIDoHeader component

--- src/Components/Shared/MyTools/WhatDoIDoMenu/WhatDoIDoMenu.jsx
+++ src/Components/Shared/MyTools/WhatDoIDoMenu/WhatDoIDoMenu.jsx
@@ -1,32 +1,15 @@
-import { useDispatch, useSelector } from "react-redux";
-import { updateGlobalState } from "src/Features/globalSlice";
-import SvgIcon from "../../MiniComponents/SvgIcon";
+import { useSelector } from "react-redux";
 import WhatDoIDoContent from "../WhatDoIDoContent/WhatDoIDoContent";
+import WhatDoIDoHeader from "../WhatDoIDoHeader/WhatDoIDoHeader";
 import s from "./WhatDoIDoMenu.module.scss";
 
 const WhatDoIDoMenu = () => {
   const { isWhatDoIDoMenuActive } = useSelector((state) => state.global);
   const activeClass = isWhatDoIDoMenuActive ? s.active : "";
-  const dispatch = useDispatch();
-
-  function closeMenu() {
-    dispatch(updateGlobalState({ key: "isWhatDoIDoMenuActive", value: false }));
-    dispatch(updateGlobalState({ key: "isOverlayActive", value: false }));
-  }
 
   return (
     <div className={`${s.menu} ${activeClass}`} role="menu">
-      <header>
-        <button
-          type="button"
-          className={s.closeBtn}
-          onClick={closeMenu}
-          aria-label="Close menu"
-        >
-          <SvgIcon name="xMark" />
-        </button>
-      </header>
-
+      <WhatDoIDoHeader />
       <WhatDoIDoContent />
     </div>
   );
