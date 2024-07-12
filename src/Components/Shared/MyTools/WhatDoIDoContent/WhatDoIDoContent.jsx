ce49708c26d15eb6681a3e86f435159023b4a607
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-07-12 16:01:38 +0300

    chore: separate WhatDoIDo content into WhatDoIDoContent component

---
+++ src/Components/Shared/MyTools/WhatDoIDoContent/WhatDoIDoContent.jsx
@@ -0,0 +1,36 @@
+import s from "./WhatDoIDoContent.module.scss";
+
+const WhatDoIDoContent = () => {
+  return (
+    <section className={s.mainContent}>
+      <div className={s.list}>
+        <h2 className={s.title}>
+          To-Do List <span className={s.todoIcon}>📒</span>
+        </h2>
+        <ul className={s.todoList}>
+          <li>Confirm receive order product button</li>
+        </ul>
+      </div>
+
+      <div className={s.list}>
+        <h2 className={s.title}>
+          Working on <span className={s.starIcon}>🌟</span>
+        </h2>
+        <ul className={s.workingOnList}>
+          <li>Build order page content</li>
+          <li>Build working on menu</li>
+        </ul>
+      </div>
+
+      <div className={s.list}>
+        <h2 className={s.title}>
+          Achieved <span className={s.fireIcon}>🔥</span>
+        </h2>
+        <ul className={s.achievedList}>
+          <li>Build order page</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+export default WhatDoIDoContent;
--- src/Components/Shared/MyTools/WhatDoIDoMenu/WhatDoIDoMenu.jsx
+++ src/Components/Shared/MyTools/WhatDoIDoMenu/WhatDoIDoMenu.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { updateGlobalState } from "src/Features/globalSlice";
 import SvgIcon from "../../MiniComponents/SvgIcon";
+import WhatDoIDoContent from "../WhatDoIDoContent/WhatDoIDoContent";
 import s from "./WhatDoIDoMenu.module.scss";
 
 const WhatDoIDoMenu = () => {
@@ -26,35 +27,7 @@ const WhatDoIDoMenu = () => {
         </button>
       </header>
 
-      <section className={s.mainContent}>
-        <div className={s.list}>
-          <h2 className={s.title}>
-            To-Do List <span className={s.todoIcon}>📒</span>
-          </h2>
-          <ul className={s.todoList}>
-            <li>Confirm receive order product button</li>
-          </ul>
-        </div>
-
-        <div className={s.list}>
-          <h2 className={s.title}>
-            Working on <span className={s.starIcon}>🌟</span>
-          </h2>
-          <ul className={s.workingOnList}>
-            <li>Build order page content</li>
-            <li>Build working on menu</li>
-          </ul>
-        </div>
-
-        <div className={s.list}>
-          <h2 className={s.title}>
-            Achieved <span className={s.fireIcon}>🔥</span>
-          </h2>
-          <ul className={s.achievedList}>
-            <li>Build order page</li>
-          </ul>
-        </div>
-      </section>
+      <WhatDoIDoContent />
     </div>
   );
 };
