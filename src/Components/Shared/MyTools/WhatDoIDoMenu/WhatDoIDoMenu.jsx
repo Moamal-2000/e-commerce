da594927d5803b4045a645bcb2c48c456b959fa2
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-07-12 15:58:24 +0300

    style: WhatDoIDoMenu component

--- src/Components/Shared/MyTools/WhatDoIDoMenu/WhatDoIDoMenu.jsx
+++ src/Components/Shared/MyTools/WhatDoIDoMenu/WhatDoIDoMenu.jsx
@@ -15,18 +15,46 @@ const WhatDoIDoMenu = () => {
 
   return (
     <div className={`${s.menu} ${activeClass}`} role="menu">
-      <button
-        type="button"
-        className={s.closeBtn}
-        onClick={closeMenu}
-        aria-label="Close menu"
-      >
-        <SvgIcon name="xMark" />
-      </button>
+      <header>
+        <button
+          type="button"
+          className={s.closeBtn}
+          onClick={closeMenu}
+          aria-label="Close menu"
+        >
+          <SvgIcon name="xMark" />
+        </button>
+      </header>
 
-      <p className={s.title}></p>
+      <section className={s.mainContent}>
+        <div className={s.list}>
+          <h2 className={s.title}>
+            To-Do List <span className={s.todoIcon}>📒</span>
+          </h2>
+          <ul className={s.todoList}>
+            <li>Confirm receive order product button</li>
+          </ul>
+        </div>
 
-      <p className={s.description}>Still working on the menu.</p>
+        <div className={s.list}>
+          <h2 className={s.title}>
+            Working on <span className={s.starIcon}>🌟</span>
+          </h2>
+          <ul className={s.workingOnList}>
+            <li>Build order page content</li>
+            <li>Build working on menu</li>
+          </ul>
+        </div>
+
+        <div className={s.list}>
+          <h2 className={s.title}>
+            Achieved <span className={s.fireIcon}>🔥</span>
+          </h2>
+          <ul className={s.achievedList}>
+            <li>Build order page</li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 };
