9c99077513c323fef3c69d7d7974d1fd89f5825c
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-07-27 20:39:57 +0300

    fix: hide PaymentCards tooltips for screen sizes less than 992 pixels

--- src/Components/CheckoutPage/PaymentSection/PaymentCards.jsx
+++ src/Components/CheckoutPage/PaymentSection/PaymentCards.jsx
@@ -1,14 +1,17 @@
+import { SCREEN_SIZES } from "src/Data/globalVariables";
 import { paymentCards } from "src/Data/staticData";
 import ToolTip from "../../Shared/MiniComponents/ToolTip";
 import s from "./PaymentCards.module.scss";
 
 const PaymentCards = () => {
+  const isLaptopScreen = window.innerWidth >= SCREEN_SIZES.laptop;
+
   return (
     <div className={s.images}>
       {paymentCards.map(({ img, alt, link, id }) => (
         <a key={id} href={link} target="_blank">
           <img src={img} alt={alt + "image"} />
-          <ToolTip left="50%" top="46px" content={alt} />
+          {isLaptopScreen && <ToolTip left="50%" top="46px" content={alt} />}
         </a>
       ))}
     </div>
