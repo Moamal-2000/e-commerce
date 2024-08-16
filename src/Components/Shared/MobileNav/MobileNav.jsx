2e9ecf59d08fa30031a013e3dfc45dfc0e960e50
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-16 12:03:31 +0300

    fix: close mobile nav menu on route change

--- src/Components/Shared/MobileNav/MobileNav.jsx
+++ src/Components/Shared/MobileNav/MobileNav.jsx
@@ -1,4 +1,7 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { updateGlobalState } from "src/Features/globalSlice";
 import AuthSideBarButtons from "./AuthSideBarButtons/AuthSideBarButtons";
 import s from "./MobileNav.module.scss";
 import MobileNavCloseBtn from "./MobileNavCloseBtn/MobileNavCloseBtn";
@@ -8,6 +11,15 @@ import UserProfileSidebar from "./UserProfileSidebar/UserProfileSidebar";
 const MobileNav = () => {
   const { isMobileMenuActive } = useSelector((state) => state.global);
   const activeClass = isMobileMenuActive ? s.active : "";
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!isMobileMenuActive) return;
+
+    dispatch(updateGlobalState({ key: "isOverlayActive", value: false }));
+    dispatch(updateGlobalState({ key: "isMobileMenuActive", value: false }));
+  }, [location]);
 
   return (
     <aside className={`${s.mobileMenu} ${activeClass}`}>
