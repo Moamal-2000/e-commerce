d81f14b9691512a144525db83dae0e5d5986856c
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-26 04:01:24 +0300

    Revert "fix(deployment): registerServiceWorker has already been declared"
    This reverts commit e9dda0bd00ed89685fb5d0620067f0611e902df7.

--- public/register-pwa.js
+++ public/register-pwa.js
@@ -1,11 +1,36 @@
-export async function register() {
-  if ("serviceWorker" in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register("./sw.js");
+import { IS_PRODUCTION } from "../src/Data/constants";
+import { registerServiceWorker } from "../src/Functions/pwa";
 
-      console.log("registered", registration);
-    } catch (e) {
-      console.log("failed to register", e);
-    }
+export async function register() {
+  const isReadToRegister = navigator?.serviceWorker && IS_PRODUCTION;
+  if (!isReadToRegister) return;
+
+  try {
+    const registration = await registerServiceWorker();
+
+    return registration;
+  } catch (e) {
+    console.log("failed to register serviceWorker", e);
   }
+}
+
+export async function registerSWWithUpdate(setShowNotification) {
+  const isReadToRegister = navigator?.serviceWorker && IS_PRODUCTION;
+  if (!isReadToRegister) return;
+
+  try {
+    const registration = await registerServiceWorker();
+    if (registration.waiting) setShowNotification(true);
+
+    registration.addEventListener("updatefound", () =>
+      handleUpdateFound(registration, setShowNotification)
+    );
+  } catch (err) {
+    console.error(`Error registering service worker: ${err}`);
+  }
+}
+
+export async function registerServiceWorker() {
+  if (!navigator?.serviceWorker) return "serviceWorker is not supported";
+  navigator.serviceWorker.register("/sw.js", { type: "module" });
 }
