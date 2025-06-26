ff6ee214b2b666fb8e98d23bf490a7ef4382767a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-26 03:05:17 +0300

    fix(register-pwa): register using relative path

--- public/register-pwa.js
+++ public/register-pwa.js
@@ -1,16 +1,14 @@
-import { IS_PRODUCTION } from "../src/Data/constants";
-
 export async function register() {
-  const isReadToRegister = navigator?.serviceWorker && IS_PRODUCTION;
+  const isReadToRegister = navigator?.serviceWorker;
   if (!isReadToRegister) return;
 
   try {
-    const registration = await navigator.serviceWorker.register("./sw.js", {
+    const registration = await navigator.serviceWorker.register("/sw.js", {
       type: "module",
     });
 
     return registration;
-  } catch (e) {
-    console.log("failed to register serviceWorker", e);
+  } catch (error) {
+    console.log("failed to register serviceWorker", error);
   }
 }
