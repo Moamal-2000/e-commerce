a2bdb440d61544797b577c10dbc2941da75f0494
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-08 16:45:33 +0300

    remove(package): html-validate package

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import useStoreWebsiteDataToLocalStorage from "./Hooks/App/useStoreWebsiteDataToLocalStorage";
-import useChangeLangDirOnKeys from "./Hooks/Helper/useChangeLangDirOnKeys";
 import AppRoutes from "./Routes/AppRoutes";
 
 function App() {
   useStoreWebsiteDataToLocalStorage();
-  // useChangeLangDirOnKeys();
-
   return <AppRoutes />;
 }
 
